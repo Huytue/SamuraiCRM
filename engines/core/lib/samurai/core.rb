ececa179bcd4102f1453651ee53b4f773477baea
Huytue <bachvonam1996@gmail.com>
2020-08-05 15:55:50 +0700

    devise

---
+++ engines/core/lib/samurai/core.rb
@@ -0,0 +1,11 @@
+require 'sass-rails'
+require 'bootstrap-sass'
+require 'autoprefixer-rails'
+require 'jquery-rails'
+require 'rails-ujs'
+require 'devise'
+module Samurai
+  module Core
+    # Your code goes here...
+  end
+end
